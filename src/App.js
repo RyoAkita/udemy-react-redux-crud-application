908a30545781ed75e18fe106be00b30cce32840a
アキタリョウ <ryotennis0503@gmail.com>
2019-02-05 12:41:01 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
-
-/*
-class App extends Component {
-  render() {
-    return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-    <input type="text" onClick ={ ()=> console.log("Hi!")}/>
-    </React.Fragment>
-   
-    )
-  }
-} 
-*/
 const App = () => {
+  const profiles = [
+    {
+      name:"Taro", age: 10
+    },
+    {
+      name:"Ryo", age: 18
+    },
+    {
+      name:"Shu"
+    }
+
+  ]
   return (
-    <div>
-  <Cat />
-  <Cat />
-  <Cat />
+  <div>
+    {
+      profiles.map((profile, index) => {
+        return  <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
   </div>
   
   )
 }
 
-const Cat = () => {
-  return <div>Meow</div>
+const User = (props) => {
+  return <div>Hi, I am{props.name}, and {props.age} years old.</div>
 }
+
+User.propTypes = {
+name: PropTypes.string,
+age: PropTypes.number
+
+}
+
 
 export default App;
